cd478b90249e42b5724ae4a055abea6f73bbddd9
98141 <armandomora14115@gmail.com>
2026-07-01 20:50:29 -0500

    Modificaciones para .env en produccion

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const mongoose = require("mongoose");
 const { validateEnv } = require("./config/env");
