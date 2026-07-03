11c55f89b66f53a0260453caab1d73084cd7d2da
98141 <armandomora14115@gmail.com>
2026-07-03 12:38:36 -0500

    Modificaciones para produccion

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,6 @@
 // frontend/server.js
 // Archivo de arranque para cPanel "Setup Node.js App" (Passenger).
 // Passenger no ejecuta "npm start"/"next start"; requiere un archivo .js
-// que arranque un servidor HTTP escuchando en el PORT que él asigna.
 const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
